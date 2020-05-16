f85ec6a513aba5f5d8eee3f0b804c1d455f80495
rdowd <rjarrettdowd@yahoo.com>
2020-05-15 20:29:56 -0400

    Added tests for login route. will write out login functionality next.

--- __tests__/auth-router.js
+++ __tests__/auth-router.js
@@ -10,7 +10,7 @@ afterAll(async () => {
     await db.destroy()
 })
 
-describe("users authentication intergation test", () => {
+describe("users registration intergation test", () => {
     it("POST /api/auth/register registers a user", async () => {
         const newUser = { username: "test", password: "test"}
         const res = await supertest(server).post("/api/auth/register").send(newUser)
@@ -18,4 +18,42 @@ describe("users authentication intergation test", () => {
         expect(res.type).toBe("application/json")
         expect(res.body.username).toBe("test") 
     })
+
+    it("POST /api/auth/register (fail- user that already exists)", async () => {
+        const notUniqueUser = {username: "Admin", password:"adminpass"}
+        const res = await supertest(server).post("/api/auth/register").send(notUniqueUser)
+        expect(res.statusCode).toBe(409)
+
+    })
+
+
+})
+
+describe("users login intergation test", () => {
+    it("POST /api/auth/login logs in a user", async () => {
+        //add a user to hash a pass and test against that entry
+        const newUser = { username: "test", password: "test"}
+        await supertest(server).post("/api/auth/register").send(newUser)
+        //now test against newly created user
+        const res = await supertest(server).post("/api/auth/login").send(newUser)
+        expect(res.statusCode).toBe(200)
+        expect(res.type).toBe("application/json")
+        expect(res.body.username).toBe("test") 
+    })
+
+    it("POST /api/auth/login (fail- user that does not exists)", async () => {
+        const notAUser = {username: "Admin216", password:"adminpass"}
+        const res = await supertest(server).post("/api/auth/login").send(notAUser)
+        expect(res.statusCode).toBe(401)
+
+    })
+
+    it("POST /api/auth/login (fail- wrong password)", async () => {
+        const notPass = {username: "Admin", password:"wrongpass"}
+        const res = await supertest(server).post("/api/auth/login").send(notPass)
+        expect(res.statusCode).toBe(401)
+
+    })
+
+
 })
